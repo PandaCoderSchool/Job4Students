9be98e99de78d51ecc5800be13c84939c356a27f
Dan Tong <tongvtdan@outlook.com>
2015-09-20 21:07:19 +0700

    Added file for Parse
    Added Bridge header file
    Added ParseInterface to connect to Parse server

---
+++ Jobs4Students/ParseInterface.swift
@@ -0,0 +1,13 @@
+//
+//  ParseInterface.swift
+//  Jobs4Students
+//
+//  Created by Dan Tong on 9/20/15.
+//  Copyright © 2015 iOS Swift Course. All rights reserved.
+//
+
+import UIKit
+
+class ParseInterface: NSObject {
+
+}
