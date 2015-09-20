d23306b9c6d11592a5bec5780e9986c0376af644
Dan Tong <tongvtdan@outlook.com>
2015-09-20 21:31:41 +0700

    Added parseSetup to set App ID

--- Jobs4Students/ParseInterface.swift
+++ Jobs4Students/ParseInterface.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+let appId = "GVaD3r4Vyi7uQHSydphnOWW2KC4EHHAKm31GXDm7"
+let clientKey = "ICQxhc9IndnBEq5X0e4XBAvYIeULHhxzuyi0yzzS"
 class ParseInterface: NSObject {
+  
+  class var sharedInstance: ParseInterface {
+    struct Static {
+      static var instance = ParseInterface()
+    }
+    return Static.instance
+    
+  }
+  
+  override init() {
+    super.init()
+  }
+
+  func parseSetup() {
+    Parse.setApplicationId(appId, clientKey: clientKey)
+    
+  }
 
 }
