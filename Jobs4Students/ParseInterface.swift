e70966f19fef0e143fc6640c4be129223153beb8
Dan Tong <tongvtdan@outlook.com>
2015-09-20 22:51:08 +0700

    Auto Login with default userName and userPassword

--- Jobs4Students/ParseInterface.swift
+++ Jobs4Students/ParseInterface.swift
@@ -8,25 +8,94 @@
 
 import UIKit
 
-let appId = "GVaD3r4Vyi7uQHSydphnOWW2KC4EHHAKm31GXDm7"
-let clientKey = "ICQxhc9IndnBEq5X0e4XBAvYIeULHhxzuyi0yzzS"
+
 class ParseInterface: NSObject {
+  
+  // Refer to App setting on Parse: https://www.parse.com/apps/job4students/edit#keys
+  
+  let appId = "GVaD3r4Vyi7uQHSydphnOWW2KC4EHHAKm31GXDm7"
+  let clientKey = "ICQxhc9IndnBEq5X0e4XBAvYIeULHhxzuyi0yzzS"
+  
+  let defaultUserName = "panda"
+  let defaultPassword = "panda"
+  
+  // sharedInstance to be used in other classes
   
   class var sharedInstance: ParseInterface {
     struct Static {
       static var instance = ParseInterface()
     }
     return Static.instance
-    
   }
   
   override init() {
     super.init()
   }
-
+  
+  // This will be call in AppDelegate to setup Parse Application
+  
   func parseSetup() {
     Parse.setApplicationId(appId, clientKey: clientKey)
+  }
+  
+  func getJobsInformation() {
     
   }
+  
+  func parseSignUp(userName: NSString?, userPass: NSString?) -> Bool{
+    let user = PFUser()
+    user.username = userName as? String
+    user.password = userPass as? String
+    
+    var signUpIsSuccess = false
+    
+    user.signUpInBackgroundWithBlock {
+      (succeeded: Bool, error: NSError?) -> Void in
+      if let error = error {
+        let errorString = error.userInfo["error"] as? NSString
+        print(errorString)
+        signUpIsSuccess = false
+      } else {
+//        self.performSegueWithIdentifier("loginSegue", sender: self)
+        print("Sign up successful")
+        signUpIsSuccess = true
+      }
+    }
+    return signUpIsSuccess
+  }
+  
+  func parseSignIn(userName: String?, userPass: String?) -> Bool {
+    var loginIsSuccess = false
+    PFUser.logInWithUsernameInBackground(userName!, password: userPass!) { (user: PFUser?, err: NSError?) -> Void in
+      
+      if user != nil {
+        
+        loginIsSuccess = true
+        
+      } else {
+        
+        loginIsSuccess = false
+        
+        if let error = err {
+          let errStr = error.userInfo["user"] as? NSString
+          print("Error: \(errStr)")
+        }
+      }
+    }
+    return loginIsSuccess
+  }
+  
+  // This function will check is the current user is already login, go to next step, if not, show login or sign up
+  func isLogInPrevious() -> Bool {
+    let currentUser = PFUser.currentUser()
+    if currentUser != nil {
+      print("Log in already, go to next")
+      return true
+    } else {
+      // Show the signup or login screen
+      print("User should Login or signup")
+      return false
+    }
 
-}
+  }
+}  // End of Class
--- Jobs4Students/ViewController.swift
+++ Jobs4Students/ViewController.swift
@@ -15,11 +15,24 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
+  override func viewDidAppear(animated: Bool) {
+    if ParseInterface.sharedInstance.isLogInPrevious() {
+      print("Already login. Show main screen")
+    }else {
+      print("User must sign up first then login again")
+      if ParseInterface.sharedInstance.parseSignUp(ParseInterface.sharedInstance.defaultUserName, userPass: ParseInterface.sharedInstance.defaultPassword) {
+        ParseInterface.sharedInstance.parseSignIn(ParseInterface.sharedInstance.defaultUserName , userPass: ParseInterface.sharedInstance.defaultPassword)
+        
+      }
+      
+    }
+  }
 
 }
 
