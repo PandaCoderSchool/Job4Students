a344c533ca961555b4487f5a190fe43436c9e5b2
Dan Tong <tongvtdan@outlook.com>
2015-09-21 08:03:31 +0700

    Added JobCell
    Add JobCell class

---
+++ Jobs4Students/JobCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobCell.swift
+//  Jobs4Students
+//
+//  Created by Dan Tong on 9/21/15.
+//  Copyright © 2015 iOS Swift Course. All rights reserved.
+//
+
+import UIKit
+
+class JobCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
